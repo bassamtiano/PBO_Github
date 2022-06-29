acfa5bbbb1d86127bea8c36178bca6c04e1d0d3b
bassamtiano <bassamtiano@gmail.com>
2022-06-29 10:09:39 +0700

    Adding Form Test GUI

--- src/pbo_github/PBO_Github.java
+++ src/pbo_github/PBO_Github.java
@@ -14,7 +14,7 @@ public class PBO_Github {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new TestGUI().setVisible(true);
     }
     
 }
